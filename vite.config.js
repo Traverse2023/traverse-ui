509625cf7531a3c7bf0cd14555cef52593c12c0e
wiktor-falek <falekwiktor@gmail.com>
2024-02-05 18:53:33 +0100

    move browserlist to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 
 export default defineConfig({
     base: "",
@@ -8,5 +9,12 @@ export default defineConfig({
     server: {
         open: true,
         port: 3000,
+    },
+    build: {
+        target: browserslistToEsbuild([
+            ">0.2%",
+            "not dead",
+            "not op_mini all"
+        ]),
     },
 });
