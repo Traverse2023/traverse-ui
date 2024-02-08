f402ebb6a443d5205505aabf51aff7121d71cb3e
wiktor-falek <falekwiktor@gmail.com>
2024-02-08 04:31:04 +0100

    transform require to import, open browser only in development env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 
-export default defineConfig({
-    base: "",
-    plugins: [react(), viteTsconfigPaths()],
-    server: {
-        open: true,
-        port: 3000,
-    },
-    build: {
-        target: browserslistToEsbuild([
-            ">0.2%",
-            "not dead",
-            "not op_mini all"
-        ]),
-    },
+export default defineConfig(({ mode }) => {
+    const { NODE_ENV } = loadEnv(mode, process.cwd(), "");
+
+    return {
+        base: "",
+        plugins: [react(), viteTsconfigPaths()],
+        server: {
+            open: NODE_ENV === "development",
+            port: 3000,
+        },
+        build: {
+            target: browserslistToEsbuild([
+                ">0.2%",
+                "not dead",
+                "not op_mini all",
+            ]),
+            commonjsOptions: {
+                transformMixedEsModules: true,
+            },
+        },
+    };
 });
