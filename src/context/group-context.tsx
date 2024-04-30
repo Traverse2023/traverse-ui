dfe81e803fd5272f86afc6233876bfae0171b530
Isfar Oshir <isfaroshir@gmail.com>
2024-04-30 15:43:42 -0400

    added a promise to check if anyone is still speaking & break

--- src/context/group-context.tsx
+++ src/context/group-context.tsx
@@ -32,7 +32,7 @@ interface GroupContextType {
     agoraConfig: any
     setAgoraConfig: () => {}
     speakerUid: number | null
-    setSpeakerUid: (uid: UID) => {}
+    setSpeakerUid: (uid: UID | null) => {}
     currentUserUid: number | null
     setCurrentUserUid: (uid: UID) => {}
 }
