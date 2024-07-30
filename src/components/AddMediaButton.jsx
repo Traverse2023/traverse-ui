f8659aeca3df75126748ffa7c03d1d925d1299a8
rezwans1027 <rezwans1027@gmail.com>
2024-07-30 01:27:35 -0400

    removed tailwind and shadcn dependencies. created media button options popup

--- src/components/AddMediaButton.jsx
+++ src/components/AddMediaButton.jsx
@@ -1,9 +1,94 @@
+import React, { useState, useEffect, useRef } from "react";
 
+const mediaOptions = [
+  {
+    name: "Code",
+    image: "path/to/code-image.png", // Add the appropriate image path
+    value: "code",
+    function: () => console.log("code"),
+  },
+  {
+    name: "Hyperlink",
+    image: "path/to/hyperlink-image.png",
+    value: "hyperlink",
+    function: () => console.log("hyperlink"),
+  },
+  {
+    name: "Photo/Video",
+    image: "path/to/photo-video-image.png",
+    value: "photo-video",
+    function: () => console.log("photo-video"),
+  },
+  {
+    name: "Audio",
+    image: "path/to/audio-image.png",
+    value: "audio",
+    function: () => console.log("audio"),
+  },
+];
 
 const AddMediaButton = () => {
-  return (
-    <button className="plus">+</button>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const optionsRef = useRef(null);
+  const buttonRef = useRef(null);
 
-export default AddMediaButton
+  const openOptions = (event) => {
+    event.stopPropagation();
+    setOpen(!open);
+  };
+
+  const selectOption = (option) => {
+    if (option && typeof option.function === "function") {
+      option.function();
+    }
+    setOpen(false);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      optionsRef.current &&
+      !optionsRef.current.contains(event.target) &&
+      buttonRef.current &&
+      !buttonRef.current.contains(event.target)
+    ) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="add-media">
+      <button
+        onClick={openOptions}
+        ref={buttonRef}
+        className="add-media-button"
+      >
+        +
+      </button>
+      {open && (
+        <div
+          className="add-media-options"
+          ref={optionsRef}
+        >
+          {mediaOptions.map((option) => (
+            <div
+              key={option.value}
+              onClick={() => selectOption(option)}
+            >
+
+              {option.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddMediaButton;
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import browserslistToEsbuild from "browserslist-to-esbuild";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
     const { NODE_ENV } = loadEnv(mode, process.cwd(), "");
